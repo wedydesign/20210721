6361063d837503cf4c493fb0dabf96c564e14251
wedydesign <yoonseopark@wedydesign.com>
2021-09-09 14:39:25 +0900

    xx

---
+++ Helloexpress/test.js
@@ -0,0 +1,22 @@
+/* createConnection (user, password); */
+const mysql = require ('mysql');
+
+let client = mysql.createConnection ({
+    host: 'localhost',
+    user: 'root',
+    password: 'qkr8dbs6tj4@',
+    database: 'mydb'
+});
+
+client.connect(); //db실행
+
+/* query ('sql', (에러에 관한 매개변수, 접속결과에 대한 매개변수)); */
+client.query ('SELECT * from portfolio', (error, result) => {
+    if(error) {
+        console.log ('error query');
+    } else {
+        console.log (result);
+    }
+});
+
+client.end(); //db종료
